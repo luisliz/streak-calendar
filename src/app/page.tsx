a031a1336b123420d1d9523c1275390c2a3ac2ab
Ilya Aizenberg <ilyaizen@gmail.com>
2024-12-29 06:53:17 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <span className="font-semibold italic">true potential</span>.
       </p>
 
-      <div className="shadow-lg border-4 border-red-500 rounded-3xl mt-16 relative w-full max-w-2xl overflow-hidden aspect-video mx-auto transition-[border-width,border-color] ease-in hover:border-[8px] hover:border-red-400">
+      <div className="shadow-lg border-4 border-red-500 rounded-3xl mt-16 relative w-full max-w-2xl overflow-hidden aspect-video mx-auto transition-[border-color] hover:border-red-400">
         <iframe
           className="absolute left-0 top-0 h-full w-full"
           src="https://www.youtube.com/embed/mVQ1bzd816I?si=h-UGS9kLXH-gN2Cs"
