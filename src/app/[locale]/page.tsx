3fe46cc79398426e05240b9cef7eebb9a0d72f56
Ilya Aizenberg <ilyaizen@gmail.com>
2025-01-08 03:56:20 +0200

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,7 +9,6 @@ import { Link } from "@/i18n/routing";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { Activity, Timer } from "lucide-react";
 import { useTranslations } from "next-intl";
-import Image from "next/image";
 
 /**
  * Home component - Landing page of the application
