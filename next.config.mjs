4e86e11651db5a39c654e976f43cc950dff47fdd
Ilya Aizenberg <ilyaizen@gmail.com>
2025-01-12 07:37:21 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 /** @type {import('next').NextConfig} */
 const config = {
   typescript: {
+    // TODO: 2025-01-12 - is that a good idea? calendar-details.tsx is not typed or something
     ignoreBuildErrors: true,
   },
 };
